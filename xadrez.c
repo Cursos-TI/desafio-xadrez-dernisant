24a052bdf3ba76f9cd9f54aa75d9af96e5d2871d
Dernival Santana Santos <104606762+dernisant@users.noreply.github.com>
2025-09-22 22:40:04 +0000

    Implementada movimentação do cavalo

--- xadrez.c
+++ xadrez.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     // Movendo a torre 5 casas a direita
+
+    printf("Movimentação da torre: \n");
+
     for (int t = 0; t < 5; t++)
     {
         printf("Direita\n"); // Imprime a direção do movimento
     }
     
-    // Movendo o bispo 5 casas na diagona na direita para cima
+    printf("\n");
+
+    // Movendo o bispo 5 casas na diagonal na direita para cima
 
     int b = 0;
     
+    printf("Movimentação do bispo: \n");
+
     do
     {
         printf("Cima, Direita\n"); // Imprime a direção do movimento
         b++;
     } while (b < 5);
 
+    printf("\n");
+    
     // Movendo a rainha 8 casas à esquerda
 
     int r = 0;
+
+    printf("Movimentação da rainha: \n");
 
     while(r < 8){
 
@@ -27,5 +38,25 @@ int main() {
         r++;
     }
     
-    return 0;
+    printf("\n");
+
+    //Movendo o cavalo em L(duas vezes para baixo e uma para a esquerda)
+    
+    int movimentocompleto = 1;
+
+    printf("Movimentação do cavalo: \n");
+
+    do{
+
+        for(int i = 1; i <= 1; i++){
+        
+            printf("Baixo \n");    
+            
+        }
+        
+    }while(movimentocompleto--);
+    
+    printf("Esquerda \n");
+    
+return 0;
 }
